d36fe34c84868f5aa19b477948f371d8abbc38cd
FawziaAlushrafii <Fawzia.Hassan.ruh.k@tuwaiq.edu.sa>
2021-10-20 03:16:30 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+
+fun main() {
+//create a map 3 elements
+
+
+    val cars = mutableMapOf(
+        "car1" to "Mercedes", "car2" to "Lexus",
+        "car3 " to "Audi"
+
+    )
+    //create list
+
+    val colorcara = mutableListOf(" car1" to "Black", "car2" to "white", "car3" to "silver")
+    println(colorcara)
+    //Add one element
+    colorcara.add() //whyyyy?? 
+    //Add one element
+
+
+
+}
